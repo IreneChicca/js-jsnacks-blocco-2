2e76a7ad1bcb35c9140eb801f6d9d317808d9106
IreneChicca <irene.chicca@outlook.com>
2023-07-25 14:43:15 +0200

    JSnack 1

--- Snack1/main.js
+++ Snack1/main.js
@@ -1,1 +1,28 @@
-alert("prova")
+
+
+
+
+// - Attraverso un prompt chiedo un numero all'utente 
+
+let userNumber = prompt("Inserisci un numero")
+
+// seleziono elemento di interesse
+
+const showNumber = document.getElementById("show-number")
+
+// - Imposto una condizione dove *SE* - il numero è pari viene stampato sulla pagina - se il numero è dispari aggiungerò un incremento di +1 per stampare il numero successivo
+
+if (userNumber % 2 == 0) {
+
+    showNumber.innerHTML += userNumber
+
+
+}
+
+else {
+    userNumber++;
+
+
+    showNumber.innerHTML += userNumber
+}
+
